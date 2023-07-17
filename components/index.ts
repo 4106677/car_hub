dbf82c233b06516915cb252efb1e4af98b6be216
Yaroslav <4106677@gmail.com>
2023-07-17 17:39:50 +0300

    add footer, header and hero

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+
+export { Hero, CustomButton, Footer, NavBar };
