a84fd8a56faf8a4ecb7f8b21fa068dbab403730b
Yaroslav <4106677@gmail.com>
2023-07-18 16:22:45 +0300

    add cars filter

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import NavBar from "./NavBar";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Footer, NavBar };
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
